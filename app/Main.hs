6aded5f7097391520405ee6a46f60194d540fef4
Omar Mefire <omefire@gmail.com>
2019-03-19 18:10:46 +0100

    WIP: Now, emails can be sent successfully via AWS SES, granted we need to set in access + secret keys manually within the code.

--- app/Main.hs
+++ app/Main.hs
@@ -11,6 +11,9 @@ import ConnectionInfo as CI
 import Control.Exception
 import Control.Monad.IO.Class (liftIO)
 import Database.PostgreSQL.Simple
+import Email as Email
+import Data.Text as T
+import System.IO
 
 data MailerException = DBConnectionIssue !String
   deriving (Show)
@@ -20,8 +23,9 @@ instance Exception MailerException
 main :: IO ()
 main = do
   forever $ do
+    hSetBuffering stdout NoBuffering
     currentTime <- getCurrentTime
-    -- putStrLn $ show currentTime
+    putStrLn $ show currentTime
     let threeMins = 3 * 60
     let beginDate = addUTCTime (- threeMins) currentTime -- now() - 3 mins
     let endDate = addUTCTime (threeMins) currentTime  -- now() + 3 mins
@@ -36,8 +40,9 @@ main = do
                                              ,connectUser = user connInfo
                                              }
         reminders <- getReminders conn beginDate endDate
-        -- putStrLn $ show $ length reminders
-        mapM_ sendEmail reminders
+        putStrLn $ show $ Prelude.length reminders
+        (flip mapM) reminders $ \reminder -> Email.sendEmail (Email.From "Trello Reminders <info@trelloreminders.com>") (Prelude.map (\re -> Email.To (T.pack $ re)) (reminderEmails reminder)) reminder
+        -- mapM_ Main.sendEmail reminders
         -- TODO: Mark emails as processed once completed
         -- TODO: What if email sending fails?
         -- TODO: What if this program crashes? Should we reload all reminders that should have been processed in the past, but weren't?
@@ -48,7 +53,8 @@ main = do
         -- TODO: Test: while this program is already running, make sure to add a new reminder to the db that should execute in the next 5 mins and see if it gets processed correctly
         -- TODO: Test: while this program is already running, make sure to add a new reminder to the db that should execute in the next 3 mins and see if it gets processed correctly
         -- TODO: Brainstorm other what/if scenarios
-        threadDelay 300000
+        putStrLn "ONE LOOP DONE!"
+        threadDelay 30000
 
 -- TODO: After email is sent successfully, mark it as processed in the database
 sendEmail :: Reminder -> IO ()
